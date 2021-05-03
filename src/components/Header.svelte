50c1890d42fb7e3ce9bca7a7a93cfa27c32ac992
miyasan_0301 <miyasan.19990301@gmail.com>
2021-05-03 13:06:13 +0900

    💡 add:buton,input,header components

---
+++ src/components/Header.svelte
@@ -0,0 +1,10 @@
+<header class="flex justify-center items-center w-full p-4 bg-green-300">
+
+    <a href="/#" class="flex lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span class="ml-3 text-xl">Qin Discussion</span>
+    </a>
+
+</header>
