455dc9440f540cfbe8bed59532e01c9b586e1149
miyasan_0301 <miyasan.19990301@gmail.com>
2021-05-05 11:21:16 +0900

    📝  update

--- src/Router.svelte
+++ src/Router.svelte
@@ -15,14 +15,25 @@
 <script lang="ts">
 import { Router, Route } from 'svelte-routing';
 import { Header } from './components';
-import Discussion from './pages/Discussion.svelte';
-import Talking from './pages/Talking.svelte';
-import Admin from './pages/Admin.svelte';
-import Setting from './pages/Setting.svelte';
-import NotFound from './pages/NotFound.svelte';
+import { Discussion, Talking, Admin, Setting, NotFound } from './pages';
 </script>
 
 <Router>
+  <div class="fixed w-full">
+    <div class="top-0">
+      <Header />
+    </div>
+    <div class="top-20">
+      <Route path="/"><Discussion /></Route>
+      <Route path="/talking/:id"><Talking /></Route>
+      <Route path="/admin"><Admin /></Route>
+      <Route path="/setting"><Setting /></Route>
+      <Route path="*"><NotFound /></Route>
+    </div>
+  </div>
+</Router>
+
+<!-- <Router>
   <Header />
   <div>
     <Route path="/" component={Discussion} />
@@ -31,4 +42,4 @@ import NotFound from './pages/NotFound.svelte';
     <Route path="/setting" component={Setting} />
     <Route path="*" component={NotFound} />
   </div>
-</Router>
+</Router> -->
