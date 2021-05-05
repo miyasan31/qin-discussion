ee6f80717e789eaf65b67c17f46467597ebeac58
miyasan_0301 <miyasan.19990301@gmail.com>
2021-05-05 15:40:27 +0900

    💄 update: type

--- src/components/TextInput.svelte
+++ src/components/TextInput.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+export let type: 'text' | 'name' | 'password';
 export let value: string;
+export let error: boolean;
 </script>
 
-<textarea
-  bind:value
-  type="text"
-  rows="3"
-  placeholder="お題を入力"
-  class="input input-primary input-bordered w-full h-60 resize-none" />
+{#if type === 'text'}
+  <textarea
+    bind:value
+    type="text"
+    rows="3"
+    placeholder="お題を入力"
+    class="input input-primary input-bordered w-full h-60 resize-none" />
+{:else if type === 'name'}
+  <input
+    bind:value
+    type="text"
+    placeholder="匿名の場合は未入力"
+    class="input input-primary input-bordered w-full mt-2" />
+{:else if type === 'password'}
+  <input
+    bind:value
+    type="password"
+    placeholder="管理者パスワード"
+    class={`input input-primary input-bordered w-full ${error && 'bg-red-200'}`} />
+{/if}
