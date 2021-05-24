bea37541f2536baad6ea42b960463991ead1be30
miyasan_0301 <miyasan.19990301@gmail.com>
2021-05-24 10:41:09 +0900

    💄 update:style-code refactor

--- src/Tailwindcss.svelte
+++ src/Tailwindcss.svelte
@@ -1,5 +1,19 @@
 <style global lang="postcss">
 @tailwind base;
 @tailwind components;
+.tab-base {
+  @apply flex-1 py-2.5  box-border text-center text-lg text-gray-400;
+  border-bottom: 1px solid #00000022;
+}
+.tab-active {
+  @apply font-bold border-b-2 border-primary-focus text-gray-700;
+}
+.title-list {
+  @apply py-5 px-10 text-2xl font-bold hover:bg-gray-100;
+  border-bottom: 1px solid #00000022;
+}
+.error-input {
+  @apply bg-red-200;
+}
 @tailwind utilities;
 </style>
