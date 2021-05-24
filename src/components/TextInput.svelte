a1d56fcb0f88b194dda8da73fdcb12ea4a276d60
miyasan_0301 <miyasan.19990301@gmail.com>
2021-05-24 10:47:53 +0900

    💄 update:ui

--- src/components/TextInput.svelte
+++ src/components/TextInput.svelte
@@ -10,17 +10,21 @@ export let error: boolean;
     type="text"
     rows="3"
     placeholder="お題を入力"
-    class={`input input-primary input-bordered w-full h-32 resize-none ${error && 'bg-red-200'}`} />
+    class="input input-primary input-bordered w-full h-32 resize-none mb-2.5"
+    class:error-input={error} />
 {:else if type === 'name'}
   <input
+    autofocus
     bind:value
     type="text"
-    placeholder="お名前（未入力の場合は匿名）"
-    class="input input-primary input-bordered w-full mt-2.5" />
+    placeholder="お名前（未入力の場合は匿名参加）"
+    class="input input-primary input-bordered w-full" />
 {:else if type === 'password'}
   <input
+    autofocus
     bind:value
     type="password"
     placeholder="管理者パスワード"
-    class={`input input-primary input-bordered w-full ${error && 'bg-red-200'}`} />
+    class="input input-primary input-bordered w-full"
+    class:error-input={error} />
 {/if}
