f1b8503947983d5e9ef517f54a7855496142b60d
miyasan_0301 <miyasan.19990301@gmail.com>
2021-05-24 10:45:10 +0900

    📝 update:bundle files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from './Header.svelte';
-export { default as Button } from './Button.svelte';
 export { default as TextInput } from './TextInput.svelte';
 export { default as Progress } from './Progress.svelte';
+export { default as Thread } from './Thread.svelte';
+export { default as AddPostButton } from './AddPostButton.svelte';
