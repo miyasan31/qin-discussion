c7fb0b0d7ffc1fe8901ff938ac5ebaa2bf4006ef
miyasan_0301 <miyasan.19990301@gmail.com>
2021-05-27 01:31:09 +0900

    📝 add:store thread,modal toggle

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,3 +2,5 @@ import { writable } from 'svelte/store';
 
 export const name = writable('');
 export const admin = writable(false);
+export const thread = writable(false);
+export const modal = writable(false);
