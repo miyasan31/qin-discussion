d912182354a6313953e3505823cf78f6c29acc87
miyasan_0301 <miyasan.19990301@gmail.com>
2021-05-27 01:26:24 +0900

    📝 add:Style.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,10 @@
 import { Auth } from './pages';
 import Router from './Router.svelte';
 import Tailwindcss from './Tailwindcss.svelte';
+import Style from './Style.svelte';
 </script>
 
+<Style />
 <Tailwindcss />
 
 <div data-theme="root" class="none sm:block">
---
+++ src/Style.svelte
@@ -0,0 +1,85 @@
+<style global>
+[data-theme='root'] {
+  --p: 174 59% 51%;
+  --pf: 174 59% 41%;
+  --pc: 0 0% 100%;
+  --s: 41 100% 57%;
+  --sf: 41 74% 48%;
+  --sc: 0 0% 100%;
+  --a: 0 100% 63%;
+  --af: 0 60% 51%;
+  --ac: 0 0% 100%;
+  --n: 211 14% 61%;
+  --nf: 210 9% 40%;
+  --nc: 0 0% 100%;
+  --b1: 0 0% 100%;
+  --b2: 210 20% 98%;
+  --b3: 216 12% 84%;
+  --bc: 215 28% 17%;
+  --in: 207 90% 54%;
+  --su: 174 100% 29%;
+  --wa: 36 100% 50%;
+  --er: 14 100% 57%;
+}
+.main-height {
+  height: calc(100vh - 61px);
+}
+.main-h {
+  height: calc(100vh - 111px);
+}
+.border-1 {
+  border: 1px solid #00000022;
+}
+.border-t-1 {
+  border-top: 1px solid #00000022;
+}
+.border-t-1-none {
+  border-top: 1px solid #00000000;
+}
+.border-b-1 {
+  border-bottom: 1px solid #00000022;
+}
+.border-l-1 {
+  border-left: 1px solid #00000022;
+}
+.chat-msg {
+  width: fit-content;
+}
+.resize-label::-webkit-resizer {
+  display: none;
+}
+.bar-hidden::-webkit-scrollbar {
+  display: none;
+}
+.flex-2 {
+  flex: 2;
+}
+.flex-3 {
+  flex: 3;
+}
+.flex-4 {
+  flex: 4;
+}
+</style>
+
+<!-- light (default)
+    dark
+    cupcake
+    bumblebee
+    emerald
+    corporate
+    synthwave
+    retro
+    cyberpunk
+    valentine
+    halloween
+    garden
+    forest
+    aqua
+    lofi
+    pastel
+    fantasy
+    wireframe
+    black
+    luxury
+    dracula -->
