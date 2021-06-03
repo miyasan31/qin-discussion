ce3ab760150b9c423def58a5ed5f6fdf311429f0
miyasan_0301 <miyasan.19990301@gmail.com>
2021-06-04 02:30:47 +0900

    📝 bundle:components file, pages file

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 export { default as Signin } from './Signin.svelte';
 export { default as Auth } from './Auth.svelte';
-export { default as Admin } from './Admin.svelte';
+export { default as AdminPostDelete } from './AdminPostDelete.svelte';
+export { default as AdminEvent } from './AdminEvent.svelte';
 export { default as Discussion } from './Discussion.svelte';
 export { default as NotFound } from './NotFound.svelte';
 export { default as Talking } from './Talking.svelte';
