2ffe622e13d4c65369c39276dec976017eb3cee2
miyasan_0301 <miyasan.19990301@gmail.com>
2021-06-04 02:41:50 +0900

    💄 add:phonemock ui

---
+++ src/components/PhoneMock.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { fly } from 'svelte/transition';
+</script>
+
+<div in:fly={{ y: 300, duration: 1000 }}>
+  <div class="mockup-phone">
+    <div class="camera" />
+    <div class="display">
+      <div class="artboard phone-1 artboard-demo bg-gray-100">
+        <slot />
+      </div>
+    </div>
+  </div>
+</div>
