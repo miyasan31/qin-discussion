8fb6921adcf5857916c5ed77b72a3c61b47243db
miyasan_0301 <miyasan.19990301@gmail.com>
2021-06-04 02:42:06 +0900

    💄 add:toast ui

---
+++ src/components/Toast.svelte
@@ -0,0 +1,28 @@
+<style>
+.root {
+  --toastBackground: rgba(255, 255, 255, 0.582);
+  --toastColor: #424242;
+  --toastProgressBackground: #38ccbd;
+  --toastContainerTop: auto;
+  --toastContainerBottom: 3rem;
+  --toastContainerRight: 3rem;
+  --toastContainerLeft: auto;
+}
+</style>
+
+<script>
+import { SvelteToast, toast } from '@zerodevx/svelte-toast';
+
+const options = {
+  duration: 2000, // duration of progress bar tween
+  dismissable: false, // allow dismiss with close button
+  initial: 1, // initial progress bar value
+  progress: 0, // current progress
+  reversed: true, // insert new toast to bottom of stack
+  intro: { x: 100 },
+};
+</script>
+
+<div class="root _toastContainer">
+  <SvelteToast {options} />
+</div>
