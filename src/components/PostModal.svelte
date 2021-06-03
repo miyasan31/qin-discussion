ef35a14329c4c07d4f805ca77663581d5d3f2503
miyasan_0301 <miyasan.19990301@gmail.com>
2021-06-04 02:45:06 +0900

    ♻️ css, trim()

--- src/components/PostModal.svelte
+++ src/components/PostModal.svelte
@@ -5,28 +5,26 @@ import { TextInput } from '../components';
 import { modal, event } from '../store';
 
 let error: boolean = false;
-let formData: PostsType = {
-  pid: '',
-  title: '',
-  creater_name: '',
-  create_time: {},
-  checked: false,
-};
+let title: string = '';
+let creater_name: string = '';
 
 const handleAdd = async (): Promise<void> => {
-  if (formData.title !== '') {
+  if (title.trim() !== '') {
     const ref = await db.collection('qin-salon').doc($event).collection('posts').doc();
-    const pid = ref.id;
-    formData.pid = pid;
-    const timestamp = FirebaseTimestamp.now();
-    formData.create_time = timestamp;
+    let formData: PostsType = {
+      pid: ref.id,
+      title: title.trim(),
+      creater_name: creater_name.trim(),
+      create_time: FirebaseTimestamp.now(),
+      checked: false,
+    };
     if (formData.creater_name === '') {
       formData.creater_name = '匿名さん';
     }
     db.collection('qin-salon')
       .doc($event)
       .collection('posts')
-      .doc(pid)
+      .doc(ref.id)
       .set(formData, { merge: true })
       .then(() => {
         handleReset();
@@ -39,14 +37,9 @@ const handleAdd = async (): Promise<void> => {
   }
 };
 const handleReset = (): void => {
-  formData = {
-    pid: '',
-    title: '',
-    creater_name: '',
-    create_time: {},
-    checked: false,
-  };
   modal.update((store_modal) => (store_modal = false));
+  title = '';
+  creater_name = '';
   error = false;
 };
 
@@ -59,17 +52,27 @@ const handleClose = (): void => {
 <div class="modal">
   <div class="w-full h-screen" on:click={handleClose} />
   <div class="fixed z-50 modal-box bg-gray-100">
-    <TextInput text_type="textarea" type="text" bind:value={formData.title} bind:error placeholder="お題を入力" />
+    <TextInput
+      text_type="textarea"
+      type="text"
+      color="primary"
+      bind:value={title}
+      bind:error
+      placeholder="お題を入力" />
     <TextInput
       text_type="input"
       type="text"
-      bind:value={formData.creater_name}
+      color="primary"
+      bind:value={creater_name}
       bind:error
       placeholder="お名前（未入力の場合は匿名参加）" />
     <div class="flex justify-end mt-5">
-      <button class="btn btn-primary btn-outline btn-sm" on:click={handleReset}>キャンセル</button>
-      <button class="btn btn-primary btn-sm ml-2" disabled={formData.title === '' ? true : false} on:click={handleAdd}
-        >投稿</button>
+      <button class="btn btn-primary btn-outline btn-sm hover:shadow" on:click={handleReset}>キャンセル</button>
+      <button
+        class="btn btn-primary btn-sm ml-2"
+        class:shadow={title.trim() === '' ? false : true}
+        disabled={title.trim() === '' ? true : false}
+        on:click={handleAdd}>投稿</button>
     </div>
   </div>
 </div>
