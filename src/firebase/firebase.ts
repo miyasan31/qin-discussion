64863c0d0eb644c4d3d3e254c6a1a196e9cba437
みやさん <71614432+miyasan31@users.noreply.github.com>
2022-01-06 17:39:02 +0900

    🙈

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBBx3F_ahTBz-1LoMShFwa56CR5VYmkxFU',
-  authDomain: 'qin-discussion.firebaseapp.com',
-  projectId: 'qin-discussion',
-  storageBucket: 'qin-discussion.appspot.com',
-  messagingSenderId: '917324162964',
-  appId: '1:917324162964:web:a55aa78e213150777a5181',
-  measurementId: 'G-5CNRL5M5X1',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 firebase.initializeApp(firebaseConfig);
